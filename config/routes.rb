169ea573cf79b91cd99530a5bdb4e94b588d8440
beekimrj <beekimrj@gmail.com>
2021-10-14 17:14:26 +0545

    Add fontawesome

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
       root to: 'devise/sessions#new', as: :unauthenticated_root
     end
   end
+  resources :users, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
